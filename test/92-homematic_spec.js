9ae46775bb9cabbd2ccf04c8e4826a5dc92cece5
Tristan Teufel <tristanteufel@googlemail.com>
2015-01-24 12:39:28 +0100

    first test

---
+++ test/92-homematic_spec.js
@@ -0,0 +1,33 @@
+var should = require("should");
+
+var homematicNode = require("../92-homematic.js");
+var helper = require("../helper.js");
+
+describe('ChangeNode', function() {
+
+    beforeEach(function(done) {
+        helper.startServer(done);
+    });
+
+    afterEach(function(done) {
+        helper.unload();
+        helper.stopServer(done);
+    });
+
+    describe('#homematic' , function() {
+
+	
+        it('should be loaded', function(done) {
+            var flow = [{"id":"homematicNode1","type":"homematic","name":"homematicNode","wires":[[]]}];
+            helper.load(homematicNode, flow, function() {
+                var homematicNode1 = helper.getNode("homematicNode1");
+                homematicNode1.should.have.property('name', 'homematicNode');
+                done();
+            });
+        });
+
+
+
+
+    });
+});
